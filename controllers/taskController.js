8cc26182dafefc7dbf77a7d300496bd4e447d9a0
SMNoushan <noushan.md@gmail.com>
2024-07-12 03:56:53 +0600

    added db.delete tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const getTasksCollection = require("../models/taskModel");
 
 // Retrieve all tasks
@@ -7,6 +8,17 @@ const getAllTasks = async (req, res) => {
   res.json(tasks);
 };
 
+// Delete a task by ID
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  const tasksCollection = await getTasksCollection();
+  const result = await tasksCollection.deleteOne({
+    _id: new ObjectId(id),
+  });
+  res.json(result);
+};
+
 module.exports = {
   getAllTasks,
+  deleteTask,
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllTasks } = require("../controllers/taskController");
+const { getAllTasks, deleteTask } = require("../controllers/taskController");
 
 const router = express.Router();
 
 router.get("/tasks", getAllTasks);
+router.delete("/tasks/:id", deleteTask);
 
 module.exports = router;
